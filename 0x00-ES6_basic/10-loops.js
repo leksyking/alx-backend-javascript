9425543041deaa5f6b54dde4e446cb3e459cef2f
leksyking <foogundipe@student.oauife.edu.ng>
2023-09-25 17:49:21 +0000

    fix linting in task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let newArray = [];
+  const newArray = [];
   for (const idx of array) {
     newArray.push(appendString + idx);
   }
-  
+
   return newArray;
 }
